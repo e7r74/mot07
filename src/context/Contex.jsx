543acca9a6c1f41b6b0b8df792a3723e6259533f
e7r74 <erhandzoldasov45@gmail.com>
2025-08-30 14:27:27 +0500

    contexturl

--- src/context/Contex.jsx
+++ src/context/Contex.jsx
@@ -39,12 +39,11 @@ const ContextProvider = (props) => {
 
     try {
       const response = await fetch(
-        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent`,
+        `https://generativelanguage.googleapis.com/v1beta/models/gemini-2.0-flash:generateContent?key=${API_KEY}`,
         {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',
-            'X-goog-api-key': API_KEY,
           },
           body: JSON.stringify({
             contents: updatedHistory,
