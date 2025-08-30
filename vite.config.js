31c707e9ba417105fbe3600d356d0fcebb0fac4f
Yerkhan <127944702+e7r74@users.noreply.github.com>
2025-08-30 14:11:53 +0500

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -1,17 +1,9 @@
-// import { defineConfig } from 'vite'
-// import react from '@vitejs/plugin-react'
-
-// // https://vite.dev/config/
-// export default defineConfig({
-//   plugins: [react()],
-// })
-
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/mot07',
+  base: process.env.VITE_GEMINI_API_KEY || '/mot07',
   server: {
     proxy: {
       '/gemini': {
